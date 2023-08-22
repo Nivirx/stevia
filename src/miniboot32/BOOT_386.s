90ced63cbab0b1e800cb97494668d0daa0733f53
Elaina Claus <elaina.claus@chtm.me>
2023-08-22 14:24:35 -0400

    small changes to License 🏳️‍⚧️

--- src/miniboot32/BOOT_386.s
+++ src/miniboot32/BOOT_386.s
@@ -1,4 +1,4 @@
-;    Copyright (C) 2020  Bradley Claus
+;    Copyright (C) 2023  Elaina Claus
 ;
 ;    This program is free software: you can redistribute it and/or modify
 ;    it under the terms of the GNU General Public License as published by
